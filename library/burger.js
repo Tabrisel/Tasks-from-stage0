a56cbb0875a65c4cb6e614932025061a93670ce3
Elizaveta  Zaharova <LizaZaharova99@mail.ru>
2023-08-11 07:28:45 +0300

    fix: images, feat: add script for burger as file burger.js

---
+++ library/burger.js
@@ -0,0 +1,29 @@
+window.onload = (function () {
+    const forBurger = document.querySelector(".burger");
+    const forMenu = document.querySelector(".menu");
+    const forClose = document.querySelector(".cross");
+    const forLinks = document.querySelectorAll(".item");
+    forBurger.addEventListener("click", event => {
+        forMenu.classList.add("menu_ON");
+        event.InMenu = true;
+        document.body.style.overflowY = 'hidden';
+    });
+    document.body.addEventListener("click", event => {
+
+        if (event.InMenu) return;
+        document.querySelector(".menu").classList.remove("menu_ON")
+        document.body.style.overflowY = 'visible';
+    });
+    forClose.addEventListener("click", () => {
+        forMenu.classList.remove("menu_ON");
+        document.body.style.overflowY = 'visible';
+    });
+    if (window.innerWidth <= 1024) {
+        for (let i = 0; i < forLinks.length; i += 1) {
+            forLinks[i].addEventListener("click", () => {
+                forMenu.classList.remove("menu_ON");
+                document.body.style.overflowY = 'visible';
+            })
+        }
+    }
+}());
