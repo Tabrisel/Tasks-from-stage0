fb3f428c89b4e078ff19713a1f0907fcc3759937
Elizaveta  Zaharova <LizaZaharova99@mail.ru>
2023-09-23 05:54:19 +0300

    feat: buttons to next and prev song

--- audio-player/player.js
+++ audio-player/player.js
@@ -1,9 +1,18 @@
 const button = document.querySelector(".main_button");
 const triangle = document.querySelector(".triangle");
 const pause = document.querySelector(".pause");
-const audio = new Audio("audio/Josef_Salvat_The_Drum.mp3");
-
-let isPlay = false;
+const nextBtn = document.querySelector(".btn_Right");
+const prevBtn = document.querySelector(".btn_Left");
+const titleCurrent = document.querySelector(".title");
+const picCurrent = document.querySelector(".picture_for_music");
+const folderPic = ["images/Josef_Salvat.png", "images/Blank.jpg", "images/Telepath.jpeg", "images/Little_Talks.jpg", "images/HeyBrother.jpg"];
+const authorCurrent = document.querySelector(".author");
+const titles = ["The Drum", "Lost In the Moment", "Telepath", "Little Talk", "Hey Brother"];
+const authors = ["Josef Salvat", "Blanks", "Conan Gray", "Of Monsters and Men", "Avicii"];
+const folderPaths = ["audio/Josef_Salvat_The_Drum.mp3", "audio/Blanks - Lost In The Moment.mp3", "audio/Conan Gray - Telepath.mp3", "audio/Of Monsters and Men - Little Talk.mp3", "audio/Avicii - Hey Brother.mp3"];
+let currentSong = 0;
+let audio = new Audio;
+audio.src = "audio/Josef_Salvat_The_Drum.mp3";
 
 function playAudio() {
     if (audio.paused) {
@@ -19,6 +28,28 @@ function playAudio() {
         }
 }
 
+function toNextSong() {
+    currentSong = currentSong + 1;
+    if (currentSong > titles.length - 1) {currentSong = 0}
+    titleCurrent.textContent = titles[currentSong];
+    authorCurrent.textContent = authors[currentSong];
+    picCurrent.src = `${folderPic[currentSong]}`;
+    audio.src = `${folderPaths[currentSong]}`;
+    playAudio();
+    }
+
+
+
+function toPrevSong() {
+    currentSong = currentSong - 1;
+    if (currentSong < 0) {currentSong = titles.length - 1}
+    titleCurrent.textContent = titles[currentSong];
+    authorCurrent.textContent = authors[currentSong];
+    picCurrent.src = `${folderPic[currentSong]}`;
+    audio.src = `${folderPaths[currentSong]}`;
+    playAudio();
+}
+
 /*function pauseAudio() {
     if (isPlay = true) {
     audio.pause();
@@ -29,5 +60,7 @@ function playAudio() {
 
 
 button.addEventListener("click", playAudio);
-button.addEventListener("click", pauseAudio);
+
+nextBtn.addEventListener("click", toNextSong);
+prevBtn.addEventListener("click", toPrevSong);
 
