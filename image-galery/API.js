a0d496f4548e89eb20d452433180af229dec14c1
Elizaveta  Zaharova <LizaZaharova99@mail.ru>
2023-09-30 06:12:07 +0300

    feat: api is working with click on search-button

--- image-galery/API.js
+++ image-galery/API.js
@@ -1,12 +1,12 @@
 const clientKey = `z67VJeSqFmgCo8zsUi_Dl-orEyCQyNIXvegOfEcsnNw`;
 const box = document.querySelector(".imagesBox");
 const array = [];
+let link = `https://api.unsplash.com/photos/random?client_id=${clientKey}&count=30&query=red`;
 
 const getPhoto = async () => {
 
-    const link = `https://api.unsplash.com/photos/random?client_id=${clientKey}&count=30&query=red`;
-    const response = await fetch(link);
-    const dataInfo = await response.json();{
+    let response = await fetch(link);
+    let dataInfo = await response.json();{
         let array = dataInfo;
         showPictures(array);
         console.log(array);
@@ -26,13 +26,37 @@ const getPhoto = async () => {
 
 getPhoto();
 
+button.onclick = function () {
 
+    let query = document.getElementById(`search_value`).value;
+    box.innerHTML = ``;
+    console.log(query);
+    link = `https://api.unsplash.com/search/photos?query=${query}&client_id=${clientKey}&per_page=30`;
+    function showPictures2(data) {
+        data.results.forEach(pic => {
+            let newElement = document.createElement("div");
+            newElement.classList.add("createdImage");
+            newElement.innerHTML = `<img src="${pic.urls.regular}" alt="generated photo" maxWidth:'300px' maxHeight:'100%'>`;
+            box.appendChild(newElement);
+            
+        });
+    }
+    const getPhoto2 = async () => {
 
+        let response = await fetch(link);
+        let dataInfo = await response.json();
+            let array = dataInfo;
+            showPictures2(array);
+            console.log(array);
+      
+    };
+    getPhoto2();
+}
 
-
-
-//  const createdImage = document.createElement("img");
-// createdImage.classList.add("image-galery");
-// createdImage.src="https://images.dog.ceo/breeds/schipperke/n02104365_2981.jpg";
-// createdImage.alt="generated_image";
-// document.querySelector(".imagesBox").append(createdImage);
+document.querySelector(".box_search").addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // код клавиши Enter
+        button.click()
+        e.preventDefault();
+    }
+});
